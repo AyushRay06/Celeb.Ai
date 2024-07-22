02f56a840760e136ffa4e36f04217522f9efb596
AyushRay06 <rayayush47@gmail.com>
2024-07-23 02:45:35 +0530

    input-fetch

--- components/search-input.tsx
+++ components/search-input.tsx
@@ -7,6 +7,9 @@ import { useRouter, useSearchParams } from "next/navigation"
 export const SearchInput = () => {
   const router = useRouter()
   const searchParams = useSearchParams()
+  const categoryId = searchParams.get("categoryId")
+  const name = searchParams.get("name")
+
   return (
     <div className="relative">
       <Search className="absolute h-4 w-4 top-3 left-4 text-muted-foreground" />
