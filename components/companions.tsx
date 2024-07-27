359a11bebe97cc184335bc51dad253001d840785
AyushRay06 <rayayush47@gmail.com>
2024-07-28 02:42:14 +0530

    companion-card-comp

--- components/companions.tsx
+++ components/companions.tsx
@@ -1,5 +1,8 @@
 import { Companion } from "@prisma/client"
 import Image from "next/image"
+import { Card, CardFooter, CardHeader } from "./ui/card"
+import Link from "next/link"
+import { MessagesSquare } from "lucide-react"
 
 interface CompanionProps {
   data: (Companion & {
@@ -13,7 +16,7 @@ export const Companions = ({ data }: CompanionProps) => {
   if (data.length === 0) {
     return (
       <div className="pt-10 flex  flex-col items-center justify-center space-y-3">
-        <div className="relative w-60 h-60">
+        <div className="relative w-80 h-80">
           <Image
             fill
             className=""
@@ -25,7 +28,38 @@ export const Companions = ({ data }: CompanionProps) => {
       </div>
     )
   }
-  return <div>Companions</div>
+  return (
+    <div className="grid grid-cols-2 sm:grid-cols-3 md:grid-cols-4 lg:grid-cols-5 xl:grid-cols-6 gap-2 pb-10">
+      {data.map((item) => (
+        <Card
+          key={item.id}
+          className="bg-primary/10 rounded-xl cursor-pointer hover:opacity-75 transition border-0"
+        >
+          <Link href={`/chat/${item.id}`}>
+            <CardHeader className="flex items-center justify-center text-center text-muted-foreground">
+              <div className="relative w-32 h-32">
+                <Image
+                  alt="Companion Image"
+                  src={item.src}
+                  fill
+                  className="rounded-xl object-cover"
+                />
+              </div>
+              <p className="font-bold">{item.name}</p>
+              <p className="text-xs">{item.description}</p>
+            </CardHeader>
+            <CardFooter className="flex items-center justify-between text-xs text-muted-foreground">
+              <p className="lowercase">@{item.userName}</p>
+              <div className="flex items-center">
+                <MessagesSquare className="w-4 h-4 mr-1" />
+                {item._count.messages}
+              </div>
+            </CardFooter>
+          </Link>
+        </Card>
+      ))}
+    </div>
+  )
 }
 
 export default Companions
