3ad12122820e84e658d893b6d3bbf7d1e56502a4
AyushRay06 <rayayush47@gmail.com>
2024-07-28 22:08:22 +0530

    Companion delete functionality

--- components/chat-header.tsx
+++ components/chat-header.tsx
@@ -37,10 +37,13 @@ export const ChatHeader = ({ companion }: ChatHeaderProps) => {
 
   const onDelete = async () => {
     try {
-      await axios.delete(`api/companions/${companion.id}`)
+      await axios.delete(`/api/companion/${companion.id}`)
       toast({
         description: "Companion Deleted Successfully.",
       })
+
+      router.refresh()
+      router.push("/")
     } catch (error) {
       toast({
         description: "Something went wrong.",
