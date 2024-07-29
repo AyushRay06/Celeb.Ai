a5168029a14f58dcb0d9b51c6b506f02379b3450
AyushRay06 <rayayush47@gmail.com>
2024-07-30 02:26:14 +0530

    fakeloading on first message

--- components/chat-messages.tsx
+++ components/chat-messages.tsx
@@ -2,6 +2,7 @@
 
 import { Companion } from "@prisma/client"
 import { ChatMessage, ChatMessageProps } from "./chat-message"
+import { useEffect, useState } from "react"
 
 interface ChatMessagesProps {
   messages: ChatMessageProps[]
@@ -14,9 +15,20 @@ export const ChatMessages = ({
   isLoading,
   companion,
 }: ChatMessagesProps) => {
+  const [fakeLoading, setFakeLoading] = useState(
+    messages.length === 0 ? true : false
+  )
+
+  useEffect(() => {
+    const timeout = setTimeout(() => {
+      setFakeLoading(false)
+    }, 1000)
+  }, [])
+
   return (
     <div className="flex-1 overflow-y-auto pr-4">
       <ChatMessage
+        isLoading={fakeLoading}
         src={companion.src}
         role="system"
         content={`Hello, I am ${companion.name}, ${companion.description}`}
